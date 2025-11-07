2baeb812f75decdd776f57d47899ea54447efc71
berkebasak <berke.basak@mail.utoronto.ca>
2025-11-07 18:52:34 -0500

    main method is implemented

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,4 +1,13 @@
 package main;
 
+import ui.UI;
+import javax.swing.SwingUtilities;
+
 public class Main {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            UI app = new UI();
+            app.setVisible(true);
+        });
+    }
 }
